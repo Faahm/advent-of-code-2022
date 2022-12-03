df34630bc6f832b0b880d97f7fc599042700d91f
Faahm <fatimahussien2@gmail.com>
2022-12-03 16:04:57 +0800

    first commit

---
+++ Day 1/part1.py
@@ -0,0 +1,11 @@
+input = open("input.txt", "r")
+sum = 0
+calList = []
+for line in input:
+    try:
+        sum = sum + float(line)
+    except:
+        calList.append(sum)
+        sum = 0
+calList.sort(reverse=True)
+print(calList[0])
