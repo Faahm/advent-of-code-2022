143537f8087b8b8ea80b78efb1d976117a828497
Faahm <62914861+Faahm@users.noreply.github.com>
2022-12-08 17:25:50 +0800

    Update part2.py

--- Day4/part2.py
+++ Day4/part2.py
@@ -1,9 +1,11 @@
 count = 0
+# Credits to https://github.com/HellFireInfernoStorm
 with open('input.txt') as f:
     for line in f:
         line = [[int(n) for n in pair.split('-')] for pair in line.strip().split(',')]
         a, b = line[0][0], line[0][1]
         c, d = line[1][0], line[1][1]
+# End of Credits
         if not (b < c or d < a):
             count += 1
 
