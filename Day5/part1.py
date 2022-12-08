02a9cb42bd2706b79a593b1b3d600ede7083f5e6
Faahm <fatimahussien2@gmail.com>
2022-12-08 15:49:30 +0800

    Finish Day5

---
+++ Day5/part1.py
@@ -0,0 +1,22 @@
+with open('input.txt') as f:
+    data = f.read().splitlines()
+
+stacks = {i : [] for i in range(1,10)}
+
+for i in range(7,-1,-1):
+    for j in range(1,10):
+        c = data[i][j*4-3]
+        if c != ' ':
+            stacks[j].append(c)
+
+for line in data[10:]:
+    instructions = line.split()
+    qty = int(instructions[1])
+    start = int(instructions[3])
+    end = int(instructions[5])
+    # accesses key, gets last n elements, reverses it
+    stacks[end] += stacks[start][-qty:][::-1]
+    del stacks[start][-qty:]
+
+for stack in stacks:
+    print(stacks[stack][-1], end='')
